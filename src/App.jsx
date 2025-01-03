1ed205d116ab49f5c526c7d1212bb19ef0e585a8
Kenkenetoy <aparece.kennethjose@cec.edu.ph>
2025-01-04 02:21:36 +0800

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 // Header pages
-import Hero from './pages/HeaderComponents/Hero/Hero';
+import Hero from "./pages/HeaderComponents/Hero/Hero";
 
 // Content pages
-import Skillz from './pages/ContentComponents/Skillz/Skillz';
-import Achievement from './pages/ContentComponents/Achievement/Achievement';
-import Projects from './pages/ContentComponents/Projects/Projects';
-
+import Skillz from "./pages/ContentComponents/Skillz/Skillz";
+import Achievement from "./pages/ContentComponents/Achievement/Achievement";
+import Projects from "./pages/ContentComponents/Projects/Projects";
 
 // Footer Component
-import AboutMe from './pages/FooterComponents/AboutMe/AboutMe';
+import AboutMe from "./pages/FooterComponents/AboutMe/AboutMe";
 
 // Utility pages
-import { name } from './info';
-import ScrollToTopButton from './pages/UtilityComponents/ScrollToTopButton/ScrollToTopButton';
-
+import { name } from "./info";
+import ScrollToTopButton from "./pages/UtilityComponents/ScrollToTopButton/ScrollToTopButton";
 
 function App() {
-
-
   useEffect(() => {
     document.title = `${name.firstname} ${name.lastname}`;
-    setTimeout(() => {
-    }, 800);
+    setTimeout(() => {}, 800);
   }, []);
 
   return (
     <>
-        <>
-          <Hero />
-          <Skillz />
-          <ScrollToTopButton />
-          <Achievement />
-          <Projects />
-          <AboutMe />
-          
-        </>
+      <>
+        <BrowserRouter basename="/Portfolio-React">
+          {/* Your routes go here */}
+        </BrowserRouter>
+        <Hero />
+        <Skillz />
+        <ScrollToTopButton />
+        <Achievement />
+        <Projects />
+        <AboutMe />
+      </>
     </>
   );
 }
 
 export default App;
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/KennethAparecePortfolio/"
-})
+  base: "/Portfolio-React/", // Use the repository name as the base path
+  build: {
+    outDir: "build",
+  },
+});
