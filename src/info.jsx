dbe451dbb86869cc3495f357b50bdbdb74cb9e9c
Kenkenetoy <aparece.kennethjose@cec.edu.ph>
2025-01-04 02:58:32 +0800

    Your commit message here

--- src/info.jsx
+++ src/info.jsx
@@ -1,102 +1,83 @@
-const backgroundImageUrl = 'src/assets/images/339933234_775141431004219_8564345315126544845_n.jpg';
+const backgroundImageUrl = "./public/assets/images/hero.JPG";
 
 const name = {
-  firstname: 'Kenneth',
-  lastname: 'Aparece',
-  url: 'https://www.facebook.com/Kenoy01',
+  firstname: "Kenneth",
+  lastname: "Aparece",
+  url: "https://www.facebook.com/Kenoy01",
 };
 
 const typeWriterText = [
-  'Digital Art',
-  'Game Developer',
-  'Team Barcode former Offlaner',
+  "Digital Art",
+  "Game Developer",
+  "Team Barcode former Offlaner",
 ];
 
 const socialProfiles = [
   {
-    name: 'Facebook',
-    icon: 'lni lni-facebook',
-    url: 'https://www.facebook.com/Kenoy01',
+    name: "Facebook",
+    icon: "lni lni-facebook",
+    url: "https://www.facebook.com/Kenoy01",
   },
 ];
 
 const categories = [
   {
-    name: 'Web Development',
-    icon: 'lni lni-code',
-    title: 'Digital Artist',
+    name: "Web Development",
+    icon: "lni lni-code",
+    title: "Digital Artist",
     description:
       "Highly skilled and innovative digital artist with a passion for creating visually stunning and engaging artworks. Seeking a challenging position where I can contribute my expertise in digital artistry and contribute to the creative vision of projects.",
     skills: [
       {
-        icon: 'lni lni-adobe',
-        title: 'adobe',
+        icon: "lni lni-adobe",
+        title: "adobe",
       },
     ],
   },
   {
-    name: 'Software',
-    icon: 'lni lni-code',
-    title: 'Software Development',
+    name: "Software",
+    icon: "lni lni-code",
+    title: "Software Development",
     description:
-    "Results-driven and detail-oriented software developer with a passion for creating efficient and scalable solutions. Seeking a challenging software development position where I can leverage my expertise in [programming languages, frameworks, or technologies] to contribute to the success of innovative projects.",
+      "Results-driven and detail-oriented software developer with a passion for creating efficient and scalable solutions. Seeking a challenging software development position where I can leverage my expertise in [programming languages, frameworks, or technologies] to contribute to the success of innovative projects.",
     skills: [
       {
-        icon: 'lni lni-camera',
-        title: 'HTML5',
+        icon: "lni lni-camera",
+        title: "HTML5",
       },
       {
-        icon: 'lni lni-certificate',
-        title: 'CSS3',
+        icon: "lni lni-certificate",
+        title: "CSS3",
       },
-     
     ],
   },
   {
-    name: 'brands',
-    icon: 'lni lni-code',
-    title: 'Game Development',
+    name: "brands",
+    icon: "lni lni-code",
+    title: "Game Development",
     description:
-    "Enthusiastic and creative game developer with a passion for creating immersive and entertaining gaming experiences. Seeking a challenging role in game development where I can leverage my skills in [game development tools, programming languages, or specific areas of expertise] to contribute to the success of innovative game projects.",
+      "Enthusiastic and creative game developer with a passion for creating immersive and entertaining gaming experiences. Seeking a challenging role in game development where I can leverage my skills in [game development tools, programming languages, or specific areas of expertise] to contribute to the success of innovative game projects.",
     skills: [
       {
-        icon: 'lni lni-code',
-        title: 'Code',
+        icon: "lni lni-code",
+        title: "Code",
       },
-      
     ],
   },
-  
 ];
 
 const achievements = [
-  { word: 'positions', value: 3, unit: '' },
-  { word: 'finished projects', value: 25, unit: '' },
-  { word: 'experience', value: 3, unit: ' years' },
+  { word: "positions", value: 3, unit: "" },
+  { word: "finished projects", value: 25, unit: "" },
+  { word: "experience", value: 3, unit: " years" },
 ];
 
 const projectData = [
   {
-    title: 'GDSC Girl',
-    description: 'This is a splash art of a GDSC mascot during my activity in GDSC',
-    demoUrl: 'src/assets/images/BIG ILLUS.png' 
+    title: "Manga - Cover Page",
+    description: "This is the cover page of my Storyboarding finals.",
+    demoUrl: "src/assets/images/Illustration.png",
   },
-  {
-    title: 'Manga - Cover Page',
-    description: 'This is the cover page of my Storyboarding finals.',
-    demoUrl: 'src/assets/images/Illustration.png' 
-  },
-  {
-    title: 'A commission #1',
-    description: 'Spent an hour with this, got P500.',
-    demoUrl: 'src/assets/images/Illustration31.png' 
-  },
-  {
-    title: 'A commission #2',
-    description: 'Spent an hour with this, got P450.',
-    demoUrl: 'src/assets/images/oten.png' 
-  },
-  
 ];
 
 const aboutMeText = {
@@ -105,9 +86,9 @@ const aboutMeText = {
 };
 
 const emailConfig = {
-  serviceID: 'service_XXXXXXX',
-  templateID: 'template_XXXXXXXX',
-  userID: 'XXXXXXXXXXXX',
+  serviceID: "service_XXXXXXX",
+  templateID: "template_XXXXXXXX",
+  userID: "XXXXXXXXXXXX",
 };
 
 export {
